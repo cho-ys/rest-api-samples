a0a5ca2ef49c4fb3ca120c3ceb82b7238ada7841
Benny Cheng <bcheng@microstrategy.com>
2019-12-06 11:39:15 -0500

    removed jupyter-connector

--- python-api/mstr-data-py/connector-jupyter/workspace-settings/rake/lib/tasks/test/acceptance.rb
+++
@@ -1,5 +0,0 @@
-
-desc "test project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
-task :test do
-  good "Puts your test command here, for example mvn test or grable test: #{__FILE__}:#{__LINE__}"
-end
